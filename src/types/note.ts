ba5668906cd82294cd7738ef56197a18c6986edd
NataliiaBybyk <145537362+NataliiaBybyk@users.noreply.github.com>
2025-11-28 15:56:57 +0100

    add

--- src/types/note.ts
+++ src/types/note.ts
@@ -3,9 +3,10 @@ export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 export interface Note {
   id: string;
   title: string;
-  content: string;
   createdAt: string;
   updatedAt: string;
+  content: string;
+
   tag: NoteTag;
 }
 
